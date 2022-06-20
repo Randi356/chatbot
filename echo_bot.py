6dc058675954f99d3f90bdb5fcc8f3ed8f223413
Cute Inspire <Randywahidin321@gmail.com>
2022-06-20 09:39:53 +0700

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("")
+bot = telebot.TeleBot("1943236911:AAHM2v3QDW3MrFN7DrN_Q7uvHW07x-wUtt8")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
