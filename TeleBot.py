f4cf3644be28dd029b1ce30a91ba7d86f7d2916d
Cute Inspire <Randywahidin321@gmail.com>
2022-06-20 09:25:12 +0700

    Update TeleBot.py

--- TeleBot.py
+++ TeleBot.py
@@ -4,6 +4,8 @@ import telebot
 API_KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commads=['test])
+@bot.message_handler(commads=['test'])
 def test(message):
-  bot.reply_message              
+  bot.reply_to(message, "hello test")
+
+bot.polling()
