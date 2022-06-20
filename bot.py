334b30629a6318ffb1e2b68d5c9a95c16385200e
Cute Inspire <Randywahidin321@gmail.com>
2022-06-20 10:15:28 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+# credits by code @FFmpegNotInstalled
+# test code bot 
+
+from pyrogram import Client 
+
+bot = Client(
+    "my first projects"
+    api_id = 
+     123456,
+    api_hash = ""
+    bot_token = ""
+)
+
+@bot.on_message(filters.commad('start') & filters.private)
+def commad1(bot, message):
+    bot.send_message(message.chat.id "Hello Test")
+
+#running bot
+bot.run()
