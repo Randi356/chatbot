2ca471eaf6f01766a51eb1a2cc5b8c87dc5d945c
Cute Inspire <Randywahidin321@gmail.com>
2022-06-20 12:40:14 +0700

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -7,11 +7,22 @@ f = open('out.jpg','wb')
 f.write(urllib.request.urlopen(url).read())
 f.close()
 
-bot = telebot.TeleBot("YOUR_TOKEN_BOTFATHER")
+bot = telebot.TeleBot("1943236911:AAHM2v3QDW3MrFN7DrN_Q7uvHW07x-wUtt8")
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "i'am developer @FFmpegNotInstalled")
+
+bot.message_handler(commands=['link'])
+def link(message):
+        bot.send_message(message.chat.id, "Link https://t.me/pantekyks
+
+@bot.message_handler(commands=['photo'])
+def send_photo(message):
+    bot.send_chat_action(message.chat.id, 'upload_photo')
+    img = open('out.jpg', 'rb')
+    bot.send_photo(message.chat.id, img, reply_to_message_id=message.message_id)
+    img.close()
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
