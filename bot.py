4fb6aba887ad0d714aefc0d8942554badbfed645
Cute Inspire <Randywahidin321@gmail.com>
2022-06-20 10:30:23 +0700

    fix added

--- bot.py
+++ bot.py
@@ -11,9 +11,21 @@ bot = Client(
     bot_token = ""
 )
 
+# start
 @bot.on_message(filters.commad('start') & filters.private)
 def commad1(bot, message):
     bot.send_message(message.chat.id "Hello Test")
+
+# help
+@bot.on_message(filters.commad('help'))
+def commad2(bot, message):
+    message.reply_text("reply test")
+
+# echo chatbot
+@bot.on_message(filters.text)
+def echobot(client, message):
+    message.reply_text(message.text)
+
 
 #running bot
 print("I AM ALIVE")
